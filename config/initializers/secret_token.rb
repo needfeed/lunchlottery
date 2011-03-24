8857e222cc62a10be6b88a12ace62e4101e30cc4
Pivotal <pivotal@emo.local>
2011-03-23 18:54:37 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LunchLottery::Application.config.secret_token = 'e1a872631015743867c6b8068eff32fc7b72482ed9136a4c5077f01b7e41670a91fa049bcf233833f7a52099e7405c82b4feaab4045cf4090108ab23adf11e22'
