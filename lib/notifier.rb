2df1a2e031fd3e33158ade9afe685b192dab5882
Pivotal <pivotal@emo.local>
2011-03-23 19:58:40 -0700

    only on monday

--- lib/notifier.rb
+++ lib/notifier.rb
@@ -1,5 +1,7 @@
 class Notifier
   def self.send_reminders
+    return if Date.today.wday != 1
+
     people = YAML.load_file("#{Rails.root}/config/people.yml")
 
     until people.empty?
--- spec/lib/notifier_spec.rb
+++ spec/lib/notifier_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe Notifier do
   describe ".send_reminders" do
-    it "should send 2 emails to 8 people, in groups of 4" do
+    before do
       YAML.stub(:load_file).with("#{Rails.root}/config/people.yml") { %w(
                               1@example.com
                               2@example.com
@@ -13,11 +13,30 @@ describe Notifier do
                               7@example.com
                               8@example.com
                             ) }
+    end
+
+    after do
+      ActionMailer::Base.deliveries = []
+    end
+
+    it "should send 2 emails to 8 people, in groups of 4" do
+      monday = Date.parse("2011-03-21")
+      Date.stub(:today) { monday }
+
       Notifier.send_reminders
 
       ActionMailer::Base.deliveries.length.should == 2
       ActionMailer::Base.deliveries[0].to.length.should == 4
       ActionMailer::Base.deliveries[1].to.length.should == 4
+    end
+
+    it "should only send emails on Monday" do
+      wednesday = Date.parse("2011-03-23")
+      Date.stub(:today) { wednesday }
+
+      Notifier.send_reminders
+
+      ActionMailer::Base.deliveries.length.should == 0
     end
   end
 end
