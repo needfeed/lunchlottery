a7c3ffa33a03ad30ef76d94e00cd9b2d13778caf
Pivotal <pivotal@emo.local>
2011-03-23 20:45:59 -0700

    setting sender on emails

--- app/mailers/reminder.rb
+++ app/mailers/reminder.rb
@@ -1,6 +1,8 @@
 class Reminder < ActionMailer::Base
   def reminder(addresses)
     @addresses = addresses
-    mail(:to =>addresses, :subject => "Your lunch tomorrow")
+    mail(:to =>addresses,
+         :subject => "Your lunch tomorrow",
+         :sender => "dine@lunchlottery.com")
   end
 end
