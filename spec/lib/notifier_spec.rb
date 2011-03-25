15fb7e2473f04545818e4a0b1906c1d750d741af
Aaron Peckham <apeckham@gmail.com>
2011-03-24 21:41:42 -0700

    refactor test

--- spec/lib/notifier_spec.rb
+++ spec/lib/notifier_spec.rb
@@ -18,16 +18,12 @@ describe Notifier do
     context "when it's Monday" do
       before do
         monday = Date.parse("2011-03-21")
-
         Date.stub(:today) { monday }
       end
 
       it "should send 2 emails to 8 people, in groups of 4" do
         Notifier.send_reminders
-
-        ActionMailer::Base.deliveries.length.should == 2
-        ActionMailer::Base.deliveries[0].to.length.should == 4
-        ActionMailer::Base.deliveries[1].to.length.should == 4
+        ActionMailer::Base.deliveries.collect { |message| message.to.length }.should == [4, 4]
       end
 
       it "should shuffle the people" do
@@ -36,62 +32,24 @@ describe Notifier do
       end
 
       context "when one person would dine alone" do
-        before do
-          @people = %w(
-                              1@example.com
-                              2@example.com
-                              3@example.com
-                              4@example.com
-                              5@example.com
-                            )
-        end
-
         it "should split up the groups more evenly" do
+          @people = example_emails(5)
           Notifier.send_reminders
-
-          ActionMailer::Base.deliveries.length.should == 2
-          ActionMailer::Base.deliveries[0].to.length.should == 3
-          ActionMailer::Base.deliveries[1].to.length.should == 2
+          ActionMailer::Base.deliveries.collect { |message| message.to.length }.should == [3, 2]
         end
       end
 
       context "when two people would dine alone" do
-        before do
-          @people = %w(
-                              1@example.com
-                              2@example.com
-                              3@example.com
-                              4@example.com
-                              5@example.com
-                              6@example.com
-                            )
-        end
-
         it "should split up the groups evenly" do
+          @people = example_emails(6)
           Notifier.send_reminders
-
-          ActionMailer::Base.deliveries.length.should == 2
-          ActionMailer::Base.deliveries[0].to.length.should == 3
-          ActionMailer::Base.deliveries[1].to.length.should == 3
+          ActionMailer::Base.deliveries.collect { |message| message.to.length }.should == [3, 3]
         end
       end
 
       context "when nine people are present" do
-        before do
-          @people = %w(
-                              1@example.com
-                              2@example.com
-                              3@example.com
-                              4@example.com
-                              5@example.com
-                              6@example.com
-                              7@example.com
-                              8@example.com
-                              9@example.com
-                            )
-        end
-
         it "should split into 3 groups of 3" do
+          @people = example_emails(9)
           Notifier.send_reminders
           ActionMailer::Base.deliveries.collect { |message| message.to.length }.should == [3, 3, 3]
         end
