a0a9178d0d47f2e136caf5432b7c6819f6dcc4d1
Aaron Peckham <apeckham@gmail.com>
2011-03-24 21:23:57 -0700

    add validations to person email

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,5 +1,6 @@
 class Person < ActiveRecord::Base
   validates :email, :email => true
+  validates_uniqueness_of :email
 
   def self.all_emails
     Person.all.map &:email
--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -1,14 +1,31 @@
 require 'spec_helper'
 
 describe Person do
+  before do
+    Person.count.should == 0
+  end
+  
   describe ".all_emails" do
     it "returns all emails" do
-      Person.delete_all
-      Person.count.should == 0
       Person.create!(:email => "1@example.com")
       Person.create!(:email => "2@example.com")
       Person.count.should == 2
       Person.all_emails.should == ["1@example.com", "2@example.com"]
+    end
+  end
+  
+  describe "validations" do
+    it "requires email to look like an email address" do
+      Person.new(:email => "i dont know my email address").should_not be_valid
+    end
+
+    it "requires email to be present" do
+      Person.new(:email => nil).should_not be_valid
+    end
+
+    it "requires email to be unique" do
+      Person.create!(:email => "me@example.com")
+      Person.new(:email => "me@example.com").should_not be_valid
     end
   end
 end
